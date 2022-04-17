68506e6b973b78570ab73fee6581445215d71163
Gurzi <pgurzeda@gmail.com>
2022-04-17 19:39:10 +0200

    context delete

--- src/components/ContentContainer/Login/Login.js
+++ src/components/ContentContainer/Login/Login.js
@@ -1,5 +1,4 @@
 import React, { useState, useContext } from "react";
-import AuthContext from "../../context/authContext";
 import "./Login.css";
 import { useNavigate } from "react-router-dom";
 import axiosFresh from "axios";
